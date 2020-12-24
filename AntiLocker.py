878e26e75fc990e6e7283e8d28a191f0392c2df7
Nikita_K <43719011+us3rname19@users.noreply.github.com>
2020-12-24 17:16:05 +1000

    Update AntiLocker.py

--- AntiLocker.py
+++ AntiLocker.py
@@ -3,18 +3,20 @@ import pyautogui
 import keyboard
 from time import sleep, gmtime, strftime
 
-screenWidth, screenHeight = pyautogui.size() # Get the size of the primary monitor.
+screenWidth, screenHeight = pyautogui.size()            # Get the size of the primary monitor.
 list_m = ['0','4','8','2','6']
 
-print("Скрипт запущен!")
+print("Script is running!")
 while True:
-    currentMouseX, currentMouseY = pyautogui.position()
-    pyautogui.moveTo(currentMouseX-1, currentMouseY)
+    currentMouseX, currentMouseY = pyautogui.position() # Get X&Y coords.
+    pyautogui.moveTo(currentMouseX-1, currentMouseY)    # Move X - 1 pic
     newMouseX,newMouseY = pyautogui.position()
     
-    if currentMouseX != newMouseX and currentMouseY != newMouseY:sleep(200)
+    if currentMouseX != newMouseX and currentMouseY != newMouseY: sleep(200)
     else: 
-        pyautogui.moveTo(currentMouseX, currentMouseY)
+        pyautogui.moveTo(currentMouseX, currentMouseY)  # Move mouse back
+        
+        # If not use keyboard press alt else break
         for minute in list_m:
             if keyboard.read_key(): break
             if strftime("%M",gmtime())[1] == minute: 
