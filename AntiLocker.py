62e1d2ed73172e5b6b36e46592166306cc538a14
Nikita_K <43719011+us3rname19@users.noreply.github.com>
2020-12-25 16:56:25 +1000

    Update AntiLocker.py

--- AntiLocker.py
+++ AntiLocker.py
@@ -6,17 +6,24 @@ from time import sleep, gmtime, strftime
 screenWidth, screenHeight = pyautogui.size()            # Get the size of the primary monitor.
 list_m = ['0','4','8','2','6']
 
-def pressed_keys(e):sleep(100);keyboard.unhook(pressed_keys)
+def pressed_keys(e):sleep(60);keyboard.unhook(pressed_keys)
 
 def main():
     try:
         print("Script is running!")
         while True:
             currentMouseX, currentMouseY = pyautogui.position() # Get X&Y coords.
+            pyautogui.moveTo(currentMouseX-5, currentMouseY)    # Move X - 5p
             keyboard.hook(pressed_keys)                         # If key is down sleep 
-            newMouseX,newMouseY = pyautogui.position()          # Get new X&Y coords.
-            if currentMouseX != newMouseX and currentMouseY != newMouseY:sleep(60)
-            else:[keyboard.press_and_release('alt') for minute in list_m if strftime("%M",gmtime())[1] == minute];sleep(100)
+            newMouseX,newMouseY = pyautogui.position()
+            if currentMouseX != newMouseX and currentMouseY != newMouseY: sleep(60)
+            else: 
+                pyautogui.moveTo(currentMouseX, currentMouseY)  # Move mouse back
+                for minute in list_m:
+                    if strftime("%M",gmtime())[1] == minute:
+                        keyboard.press_and_release('alt')
+                        break
+                sleep(60)
                 
     except KeyboardInterrupt:
         pass 
